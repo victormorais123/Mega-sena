850b26df23755891ac7d64f99665b2dbf82744d9
Gabriel Viecili <gabriel.viecili@gmail.com>
2023-06-14 20:20:20 -0300

    COMMIT 01

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import os
+import random
+
+
+
+def obternumeros():
+    NumerosEscolhidos = []
+    while len(NumerosEscolhidos) < 6:
+        entrada = input(f"Digite um numero: ")
+        if entrada.isdigit() and entrada not in NumerosEscolhidos:
+            NumerosEscolhidos.append(int(entrada))
+        else:
+            print("Entrada invalida!")
+    return NumerosEscolhidos
+
+NumerosEscolhidos = obternumeros()
+
+print(NumerosEscolhidos)
+
